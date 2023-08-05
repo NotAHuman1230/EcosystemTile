7e5a1594a871d17cef9a9aaf69ad754070acae7f
NotAHuman1230 <jerichojohn@outlook.com>
2023-08-05 18:52:54 +0100

    Perlin noise

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -12,16 +12,17 @@ public class GridManager : MonoBehaviour
     [SerializeField] Vector2Int gridSize;
     [SerializeField] float cellSize;
 
-    [SerializeField] Texture2D texture;
-    new SpriteRenderer renderer;
+    [Header("Perlin Noise")]
+    [SerializeField] int perlinCellSize;
+    [SerializeField] float perlinIntensity;
+
+    Texture2D texture;
 
     private void Start()
     {
-        renderer = GetComponent<SpriteRenderer>();
         texture = generateGrid();
         Sprite sprite = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 1f / cellSize);
-        renderer.sprite = sprite;
-        Debug.Log(renderer.sprite.texture.GetPixel(100, 100));
+        GetComponent<SpriteRenderer>().sprite = sprite;
     }
     
     Texture2D generateGrid()
@@ -31,9 +32,13 @@ public class GridManager : MonoBehaviour
         rw.Create();
 
         computeShader.SetInts("resolution", gridSize.x, gridSize.y);
+        computeShader.SetInt("gridSize", perlinCellSize);
+        computeShader.SetFloat("seed", Random.Range(0f, 1f));
+        computeShader.SetFloat("intensity", perlinIntensity);
+
         computeShader.SetTexture(computeShader.FindKernel("PerlinNoise"), "result", rw);
-        computeShader.Dispatch(computeShader.FindKernel("PerlinNoise"), gridSize.x / 8, gridSize.y / 8, 1);
-        //Debug.Log("hi");
+        computeShader.Dispatch(computeShader.FindKernel("PerlinNoise"), (int)Mathf.Ceil(gridSize.x / 8f), (int)Mathf.Ceil(gridSize.y / 8f), 1);
+
         Texture2D tex = new Texture2D(gridSize.x, gridSize.y);
         RenderTexture.active = rw;
         tex.ReadPixels(new Rect(0f, 0f, rw.width, rw.height), 0, 0);
