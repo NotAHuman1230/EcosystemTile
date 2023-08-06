19c78cded48291c8d8739399d96b505124af603e
NotAHuman1230 <jerichojohn@outlook.com>
2023-08-06 21:29:32 +0100

    Water added

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -6,49 +6,78 @@ using UnityEngine.Networking;
 public class GridManager : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] ComputeShader computeShader;
+    [SerializeField] ComputeShader noiseCompute;
+    [SerializeField] ComputeShader visualCompute;
 
-    [Header("Parameters")]
+    [Header("Map")]
     [SerializeField] Vector2Int gridSize;
     [SerializeField] float cellSize;
+    [SerializeField] float waterFactor;
 
     [Header("Perlin Noise")]
     [SerializeField] int perlinCellSize;
     [SerializeField] float perlinIntensity;
 
     Texture2D humidityTexture;
+    Texture2D temperatureTexutre;
 
     private void Start()
     {
-        humidityTexture = generatePerlinNoise();
-        Sprite sprite = Sprite.Create(humidityTexture, new Rect(0f, 0f, humidityTexture.width, humidityTexture.height), new Vector2(0.5f, 0.5f), 1f / cellSize);
-        GetComponent<SpriteRenderer>().sprite = sprite;
+        generateMap();
     }
     
-    Texture2D generatePerlinNoise()
+    Texture2D renderTexTo2D(RenderTexture _renderTexture)
+    {
+        Texture2D tex = new Texture2D(gridSize.x, gridSize.y);
+        RenderTexture.active = _renderTexture;
+        tex.ReadPixels(new Rect(0f, 0f, _renderTexture.width, _renderTexture.height), 0, 0);
+        tex.Apply();
+        return tex;
+    }
+    RenderTexture generatePerlinNoise()
     {
         RenderTexture rw = new RenderTexture(gridSize.x, gridSize.y, 0);
         rw.enableRandomWrite = true;
         rw.Create();
 
-        computeShader.SetInts("resolution", gridSize.x, gridSize.y);
-        computeShader.SetInt("gridSize", perlinCellSize);
-        computeShader.SetFloat("seed", Random.Range(0f, 1f));
-        computeShader.SetFloat("intensity", perlinIntensity);
+        noiseCompute.SetInts("resolution", gridSize.x, gridSize.y);
+        noiseCompute.SetInt("gridSize", perlinCellSize);
+        noiseCompute.SetFloat("seed", Random.Range(0f, 1f));
+        noiseCompute.SetFloat("intensity", perlinIntensity);
 
-        computeShader.SetTexture(computeShader.FindKernel("PerlinNoise"), "result", rw);
-        computeShader.Dispatch(computeShader.FindKernel("PerlinNoise"), (int)Mathf.Ceil(gridSize.x / 8f), (int)Mathf.Ceil(gridSize.y / 8f), 1);
+        noiseCompute.SetTexture(noiseCompute.FindKernel("PerlinNoise"), "result", rw);
+        noiseCompute.Dispatch(noiseCompute.FindKernel("PerlinNoise"), (int)Mathf.Ceil(gridSize.x / 8f), (int)Mathf.Ceil(gridSize.y / 8f), 1);
 
-        Texture2D tex = new Texture2D(gridSize.x, gridSize.y);
-        RenderTexture.active = rw;
-        tex.ReadPixels(new Rect(0f, 0f, rw.width, rw.height), 0, 0);
-        tex.Apply();
-        return tex;
+        return rw;
+    }
+    Texture2D generateVisuals(RenderTexture _humidityMap, RenderTexture _temperatureMap)
+    {
+        RenderTexture rw = new RenderTexture(gridSize.x, gridSize.y, 0);
+        rw.enableRandomWrite = true;
+        rw.Create();
+
+        visualCompute.SetInts("resolution", gridSize.x, gridSize.y);
+        visualCompute.SetFloat("waterFactor", waterFactor);
+
+        visualCompute.SetTexture(visualCompute.FindKernel("VisualGeneration"), "result", rw);
+
+        visualCompute.SetTexture(visualCompute.FindKernel("VisualGeneration"), "humidity", _humidityMap);
+        visualCompute.SetTexture(visualCompute.FindKernel("VisualGeneration"), "temperature", _temperatureMap);
+
+        visualCompute.Dispatch(visualCompute.FindKernel("VisualGeneration"), (int)Mathf.Ceil(gridSize.x / 8f), (int)Mathf.Ceil(gridSize.y / 8f), 1);
+
+        return renderTexTo2D(rw);
     }
 
     void generateMap()
     {
+        //Create data maps
+        RenderTexture renderHumidity = generatePerlinNoise();
+        RenderTexture renderTemperature = generatePerlinNoise();
 
+        //Create visuals
+        Sprite sprite = Sprite.Create(generateVisuals(renderHumidity, renderTemperature), new Rect(0f, 0f, gridSize.x, gridSize.y), new Vector2(0.5f, 0.5f), 1f / cellSize);
+        GetComponent<SpriteRenderer>().sprite = sprite;
     }
 
     private void OnDrawGizmosSelected()
