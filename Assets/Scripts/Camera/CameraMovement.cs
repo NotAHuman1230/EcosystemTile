cca6fbcd0ee7b46dc8fe9f41dad75c08b9246782
NotAHuman1230 <jerichojohn@outlook.com>
2023-08-08 07:27:12 +0100

    Movement improvements + parameter changes

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    [Header("Parameters")]
     [SerializeField] float speed;
     [SerializeField] float scaleFactor;
+    [SerializeField] float scaleMultiplier;
     [SerializeField] Vector2 scaleRange;
 
     new Camera camera;
+    float multiplier = 1f;
 
     private void Start()
     {
         camera = GetComponent<Camera>();
+        multiplier = 1f;
     }
     private void Update()
     {
@@ -20,19 +24,33 @@ public class CameraMovement : MonoBehaviour
         scale();
     }
 
+    float pixelDistanceToUnit(float pixels)
+    {
+        float scale = (camera.orthographicSize * 2f) / camera.pixelHeight;
+        return pixels * scale;
+    }
+
     void movement()
     {
-        float x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-        float y = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+        float x = Input.GetAxisRaw("Horizontal") * pixelDistanceToUnit(speed) * Time.deltaTime;
+        float y = Input.GetAxisRaw("Vertical") * pixelDistanceToUnit(speed) * Time.deltaTime;
 
-        transform.position += new Vector3(x, y, 0f);
+        transform.position += new Vector3(x, y).normalized * pixelDistanceToUnit(speed) * Time.deltaTime;
     }
     void scale()
     {
         if (Input.GetKey(KeyCode.E))
-            camera.orthographicSize += scaleFactor * Time.deltaTime;
+        {
+            camera.orthographicSize += scaleFactor * multiplier * Time.deltaTime;
+            multiplier += scaleMultiplier * Time.deltaTime;
+        }
         else if (Input.GetKey(KeyCode.Q))
-            camera.orthographicSize -= scaleFactor * Time.deltaTime;
+        {
+            camera.orthographicSize -= scaleFactor * multiplier * Time.deltaTime;
+            multiplier += scaleMultiplier * Time.deltaTime;
+        }
+        else
+            multiplier = 1f;
 
         camera.orthographicSize = Mathf.Clamp(camera.orthographicSize, scaleRange.x, scaleRange.y);
     }
